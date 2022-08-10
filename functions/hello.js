4bc78129a6ffd4fa0bd26a4c4c81fda012e97048
Peter Shank <peter@shank.com>
2022-08-10 09:08:37 -0700

    change to async and only allow POST

--- functions/hello.js
+++ functions/hello.js
@@ -1,10 +1,18 @@
-exports.handler = async (event) => {
-  const subject = event.queryStringParameters.name || "World";
+const querystring = require("querystring");
+
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  // When the method is POST, the name will no longer be in the event’s
+  // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = querystring.parse(event.body);
+  const name = params.name || "World";
+
   return {
     statusCode: 200,
-    body: `I received a ${event.httpMethod} request.
-Hello ${subject}!
-${params}`,
+    body: `Hello, ${name}`,
   };
 };

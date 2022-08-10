8ec7d81f2a7f89a844529a5a3d917f547fee3e73
Peter Shank <peter@shank.com>
2022-08-09 19:02:33 -0700

    get the event.httpMethod

--- functions/hello.js
+++ functions/hello.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   const subject = event.queryStringParameters.name || "World";
   return {
     statusCode: 200,
-    body: `I got a request.
+    body: `I received a ${event.httpMethod}request.
 Hello ${subject}!`,
   };
 };

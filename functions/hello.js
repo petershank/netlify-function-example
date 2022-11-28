ed58726095df5cdf840420c1a28c75cc2403237c
Peter Shank <peter@shank.com>
2022-11-27 18:53:14 -0800

    changed msg to Super Crazy World

--- functions/hello.js
+++ functions/hello.js
@@ -9,7 +9,7 @@ exports.handler = async (event, context) => {
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = querystring.parse(event.body);
-  const name = params.name || "Crazy World";
+  const name = params.name || "Super Crazy World";
 
   return {
     statusCode: 200,

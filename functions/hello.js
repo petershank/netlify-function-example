6c80c4825203927200a2087f5b621fd9d6d78daf
Peter Shank <peter@shank.com>
2024-01-20 15:02:20 -0800

    how to parse body object.  needs parse.json or stringify?

--- functions/hello.js
+++ functions/hello.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
   //const name = params.line_items(0).name || "Super Crazy World";
 
   console.log(`hello, ${typeof params}`);
-  console.log(`hello, ${Object.keys(params).length}`);
+  console.log(`hello, ${Object.keys(params)[0]}`);
   // console.log(`hello ${name}`);
   return {
     statusCode: 200,

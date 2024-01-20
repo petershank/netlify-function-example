c1beca810ac1f7c8604a64b6f5492ad12b03e4c7
Peter Shank <peter@shank.com>
2024-01-20 14:35:39 -0800

    log the body ? to find the json

--- functions/hello.js
+++ functions/hello.js
@@ -9,7 +9,7 @@ exports.handler = async (event, context) => {
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = querystring.parse(event.body);
-  const name = params.line_items(0).name || "Super Crazy World";
+  //const name = params.line_items(0).name || "Super Crazy World";
 
   console.log(params);
   // console.log(`hello ${name}`);

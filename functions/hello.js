becf97257f2699b721edbe7a1682396519d46e30
Peter Shank <peter@shank.com>
2024-01-20 14:49:39 -0800

    figure out what's in the response?

--- functions/hello.js
+++ functions/hello.js
@@ -11,7 +11,8 @@ exports.handler = async (event, context) => {
   const params = querystring.parse(event.body);
   //const name = params.line_items(0).name || "Super Crazy World";
 
-  console.log(params.id);
+  console.log(`hello, ${typeof params}`);
+  console.log(`hello, ${Object.keys(params).length}`);
   // console.log(`hello ${name}`);
   return {
     statusCode: 200,

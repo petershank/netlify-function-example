2b86545309fd194326a6c8da725ac0dba52809dd
Peter Shank <peter@shank.com>
2024-01-20 16:30:35 -0800

    parsing hell

--- functions/hello.js
+++ functions/hello.js
@@ -6,7 +6,7 @@ exports.handler = async (event, context) => {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  // When the method is POST, the name will no longer be in the event’s
+  // When the method is POST,  the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = querystring.parse(event.body);
   //const name = params.line_items(0).name || "Super Crazy World";
